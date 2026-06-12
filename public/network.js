b32fe142e5de64d60ebece5a2529bed69bc7d597
fitary <fitary.designer@gmail.com>
2026-06-12 23:21:34 +0700

    0.15

--- public/network.js
+++ public/network.js
@@ -2,21 +2,14 @@
 const socket = io();
 
 const Network = {
-    myRole: '',
-    myRoom: '',
+    myRole: null, // Lưu lại role của chính mình
     
-    // Gọi hàm này để tạo phòng
     createRoom(name) { socket.emit('createRoom', name); },
-    
-    // Gọi hàm này để tham gia
     joinRoom(code, name) { socket.emit('joinRoom', { code, username: name }); },
-    
-    // Bắt đầu game
     startGame(roomCode) { socket.emit('startGame', roomCode); },
 
-    // Lắng nghe các sự kiện (callback)
-    onInit(callback) { socket.on('init', (data) => { this.myRole = data.role; this.myRoom = data.roomCode; callback(data); }); },
+    // Các hàm lắng nghe
+    onInit(callback) { socket.on('init', (data) => { this.myRole = data.role; callback(data); }); },
     onRoomUpdate(callback) { socket.on('roomUpdated', callback); },
-    onGameStart(callback) { socket.on('gameStarted', callback); },
     onSystemMsg(callback) { socket.on('systemMsg', callback); }
 };
